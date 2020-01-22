cb4cca28682589e2528010a1450f04d5c7de2a01
Atsushi-Endo <ena-choco@live.jp>
2020-01-22 18:08:39 +0900

    フォルダ名の変更及びfolders配下に移動

---
+++ resources/views/folders/create.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.common')
+@include('head')
+@include('header')
+@section('content')
+<main>
+  <div class="folder-wrap">
+    <div class="folder-content">
+      <div class="folder-header">
+        <h2>フォルダを追加する</h2>
+      </div>
+        @if($errors->any())
+        <div class="alert alert-danger">
+          <ul>
+            @foreach($errors->all() as $message)
+              <li>{{ $message }}</li>
+            @endforeach
+          </ul>
+        </div>
+        @endif
+        <form action="{{ route('folders.create') }}" method="post">
+        @csrf
+        <h3>フォルダ名</h3>
+        <div class="folder-name-wrap">
+          <input type="text" class="folder-name" name="title" id="title" value="{{ old('title') }}">
+        </div>
+        <div class="folder-add-btn-wrap">
+          <button type="submit", value="送信", class="folder-add-btn">
+        </div>
+      </form>
+    </div>
+  </div>
+</main>
+@include('footer')
+@endsection
