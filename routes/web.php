3ea530a29f05be748a967bb87f6c6f65ec12cb67
Atsushi-Endo <ena-choco@live.jp>
2020-01-22 18:09:47 +0900

    ルーティングを変更

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@
 
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 
+Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
+Route::post('/folders/create', 'FolderController@create');
+
 Route::get('/add-folder', function () {
     return view('add-folder');
 });
