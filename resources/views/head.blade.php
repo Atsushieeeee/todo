faaa808bed941f06b8fab0865e37b40c7001b1f9
Atsushi-Endo <ena-choco@live.jp>
2020-03-03 22:00:39 +0900

    moment.jsの導入

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -17,4 +17,5 @@
   <script src= "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/js-cookie@beta/dist/js.cookie.min.js"></script>
   <script type="text/javascript" src="{{ asset('/js/cookie.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('/js/moment.js') }}"></script>
 </head>
